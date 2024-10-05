3dff37cd7578fa40894e135d23da7da5f92415f2
slobodianiukillia <illia.slobodianiuk@ugla.ua>
2024-10-05 12:47:58 +0300

    Step 5: added MRC for Categories and Tasks

--- resources/views/layouts/admin-main.blade.php
+++ resources/views/layouts/admin-main.blade.php
@@ -10,26 +10,24 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary mb-2">
+<nav class="navbar navbar-expand-lg bg-body-tertiary d-flex mb-2">
     <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse d-flex justify-between" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto d-flex mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{ route('admin.index') }}">Admin panel</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{ route('admin.users.index') }}">Users</a>
                 </li>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <li>
-                        <button type="submit" class="nav-link" style="background: none; border: none; color: inherit; cursor: pointer;">
-                            Logout
-                        </button>
-                    </li>
-                </form>
             </ul>
+
+            <div class="nav-item ms-auto">
+                <form method="POST" action="{{ route('logout') }}" class="ms-auto">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Logout</button>
+                </form>
+            </div>
         </div>
     </div>
 </nav>

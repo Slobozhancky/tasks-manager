3a06ad06ac48f67a7f27b3d92e19874e3727953b
slobodianiukillia <illia.slobodianiuk@ugla.ua>
2024-10-09 17:25:17 +0300

    add: added logic for edit and delete categories

--- app/Http/Controllers/Task/CategoryController.php
+++ app/Http/Controllers/Task/CategoryController.php
@@ -63,16 +63,33 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request)
     {
-        //
+//        dd($request->all());
+
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $category = Category::findOrFail($request->id);
+
+        if (!empty($validatedData['title'])) {
+            $category->title = $validatedData['title'];
+        }
+
+        $category->save();
+
+        return redirect()->route('dashboard')->with('success', 'Category was' . $category->id . 'update!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category)
+    public function destroy(Category $category, Request $request,)
     {
-        //
+        $category = Category::findOrFail($request->id);
+        $category->delete();
+
+        return redirect()->route('dashboard')->with('success', "Category: " . $category->id . " was delete");
     }
 }
